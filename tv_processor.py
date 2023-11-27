32dd73144c56dade952e61d6b18d9133e17230db
pasinsiri <pasin_plp0206@hotmail.com>
2023-11-27 10:10:35 +0700

    rename variable

--- tv_processor.py
+++ tv_processor.py
@@ -4,21 +4,21 @@ import os
 
 FILENAME = 'aaii_bullish_pct_raw'
 path = os.path.join('content', FILENAME + '.csv')
-raw_df = pd.read_csv(path, parse_dates=['date'])
+df = pd.read_csv(path, parse_dates=['date'])
 
-raw_df = raw_df.sort_values(by='date')
-raw_df['date'] = raw_df['date'].apply(lambda d: dt.datetime.strftime(d, '%Y%m%d') + 'T')
-raw_df['volume'].fillna(0.0, inplace=True)
-raw_df = raw_df.drop_duplicates(subset=['date'], keep='last')
-# print(raw_df.head())
+df = df.sort_values(by='date')
+df['date'] = df['date'].apply(lambda d: dt.datetime.strftime(d, '%Y%m%d') + 'T')
+df['volume'].fillna(0.0, inplace=True)
+df = df.drop_duplicates(subset=['date'], keep='last')
+# print(df.head())
 
 # forward fill values
-for c in raw_df.columns:
-    raw_df[c] = raw_df[c].fillna(method='ffill')
+for c in df.columns:
+    df[c] = df[c].fillna(method='ffill')
 
 # TODO: validate data
-assert raw_df.isnull().sum().sum() == 0, 'Data contains missing values'
+assert df.isnull().sum().sum() == 0, 'Data contains missing values'
 
 export_name = FILENAME.replace('_raw', '').upper()
 export_path = os.path.join('data', export_name + '.csv')
-raw_df.to_csv(export_path, index=False, header=False)
+df.to_csv(export_path, index=False, header=False)
