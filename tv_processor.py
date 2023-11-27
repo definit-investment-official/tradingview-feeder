3c437141e760b7a2aacfaa5c72a7b74d6a925223
pasinsiri <pasin_plp0206@hotmail.com>
2023-11-27 10:07:05 +0700

    add data validation

--- tv_processor.py
+++ tv_processor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import datetime as dt
 import os
 
-FILENAME = 'aaii_ohlcv_raw'
+FILENAME = 'aaii_bullish_pct_raw'
 path = os.path.join('content', FILENAME + '.csv')
 raw_df = pd.read_csv(path, parse_dates=['date'])
 
@@ -12,7 +12,13 @@ raw_df['volume'].fillna(0.0, inplace=True)
 raw_df = raw_df.drop_duplicates(subset=['date'], keep='last')
 # print(raw_df.head())
 
-# export_name = FILENAME.replace('_raw', '').upper()
-# export_path = os.path.join('data', export_name + '.csv')
-export_path = os.path.join('data', 'AAII_BULLISH_PCT.csv')
+# forward fill values
+for c in raw_df.columns:
+    raw_df[c] = raw_df[c].fillna(method='ffill')
+
+# TODO: validate data
+assert raw_df.isnull().sum().sum() == 0, 'Data contains missing values'
+
+export_name = FILENAME.replace('_raw', '').upper()
+export_path = os.path.join('data', export_name + '.csv')
 raw_df.to_csv(export_path, index=False, header=False)
