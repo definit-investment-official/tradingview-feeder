13f8cfd80c7c8f22202030b8d2b85660a9f6df43
pasinsiri <pasin_plp0206@hotmail.com>
2023-12-02 22:03:41 +0700

    add date column

--- generate_sample.py
+++ generate_sample.py
@@ -8,7 +8,9 @@ MIN_VALUE = 10
 MAX_VALUE = 20
 
 dates = [d.date() for d in pd.date_range(START_DATE, END_DATE)]
+date_str = [dt.datetime.strftime(d, '%Y%m%d') + 'T' for d in dates]
 
 random_samples = np.random.uniform(MIN_VALUE, MAX_VALUE, size=(len(dates), 5))
 random_df = pd.DataFrame(random_samples)
+random_df.insert(0, 'date', date_str)
 random_df.to_csv('data/TEST_RANDOM.csv', index=False, header=False)
