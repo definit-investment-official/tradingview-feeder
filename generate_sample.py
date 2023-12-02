484c5553b70312912c3bab945b6a5548a5146419
pasinsiri <pasin_plp0206@hotmail.com>
2023-12-02 22:00:50 +0700

    add random dataframe generator

--- generate_sample.py
+++ generate_sample.py
@@ -4,3 +4,11 @@ import datetime as dt
 
 START_DATE = dt.date(2001, 1, 1)
 END_DATE = dt.date(2023, 11, 30)
+MIN_VALUE = 10
+MAX_VALUE = 20
+
+dates = [d.date() for d in pd.date_range(START_DATE, END_DATE)]
+
+random_samples = np.random.uniform(MIN_VALUE, MAX_VALUE, size=(len(dates), 5))
+random_df = pd.DataFrame(random_samples)
+random_df.to_csv('data/TEST_RANDOM.csv', index=False, columns=False)
