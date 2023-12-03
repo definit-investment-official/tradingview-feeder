b7fb2fde9e7f736b42ddd46a65622398b7c995e9
pasinsiri <pasin_plp0206@hotmail.com>
2023-12-03 10:49:36 +0700

    fix random sample

--- generate_sample.py
+++ generate_sample.py
@@ -10,7 +10,11 @@ MAX_VALUE = 20
 dates = [d.date() for d in pd.date_range(START_DATE, END_DATE)]
 date_str = [dt.datetime.strftime(d, '%Y%m%d') + 'T' for d in dates]
 
-random_samples = np.random.uniform(MIN_VALUE, MAX_VALUE, size=(len(dates), 5))
+random_samples = np.random.uniform(MIN_VALUE, MAX_VALUE, size=(len(dates), 4))
+sorted_samples = [sorted(row) for row in random_samples]
+
+# prep data to OHLCV 
+
 random_df = pd.DataFrame(random_samples)
 random_df.insert(0, 'date', date_str)
 random_df.to_csv('data/TEST_RANDOM.csv', index=False, header=False)
